2bc072a9c8e5a7e9d3b15f8006a69e8f4975c369
zxpl <173951428@qq.com>
2020-02-23 21:03:43 +0800

    新增加DiscoveryClient 的使用方法demo

--- springcloud_order/src/main/java/com/itmayiedu/controller/OrderController.java
+++ springcloud_order/src/main/java/com/itmayiedu/controller/OrderController.java
@@ -1,8 +1,12 @@
 package com.itmayiedu.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @Author zxp
@@ -17,7 +21,20 @@ public class OrderController {
      * 2.Feign客户端(SpringCloud自带的)
      */
     @Autowired
-    RestTemplate restTemplate; // 直接引入RestTemplate模板会报错，需要在启动类把 RestTemplate 模板的bean实例加载到该springboot应用中
+    private RestTemplate restTemplate; // 直接引入RestTemplate模板会报错，需要在启动类把 RestTemplate 模板的bean实例加载到该springboot应用中
+
+    //  DiscoveryClient 的使用方法，可以获取已经注册到注册中心的服务列表信息，只需要知道服务的名称即可
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/DiscoveryClientMember")
+    public void DiscoveryClientMember(){
+        List<ServiceInstance> instances= discoveryClient.getInstances("springcloud-member");
+        for (ServiceInstance instance : instances) {
+            System.out.println("得到会员的服务的端口号为"+instance.getPort());
+        }
+
+    }
 
 
     @RequestMapping("/getOrder")
@@ -29,5 +46,7 @@ public class OrderController {
         System.out.println("订单服务调用会员服务，"+result);
         return result;
     }
+
+
 
 }
