4b3889e54995192dca34abb3e96b49a90da86747
zxpl <173951428@qq.com>
2020-02-23 22:15:38 +0800

    模拟一个ribbon轮询请求的代码

--- springcloud_member/src/main/java/com/itmayiedu/api/AppMember.java
+++ springcloud_member/src/main/java/com/itmayiedu/api/AppMember.java
@@ -2,7 +2,10 @@ package com.itmayiedu.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @Author zxp
@@ -14,5 +17,9 @@ public class AppMember {
     public static void main(String[] args) {
         SpringApplication.run(AppMember.class, args);
     }
-
+    @Bean
+   // @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
--- springcloud_member/src/main/java/com/itmayiedu/api/Controller/MemberController.java
+++ springcloud_member/src/main/java/com/itmayiedu/api/Controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.itmayiedu.api.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @Author zxp
@@ -12,6 +18,43 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
     @Value("${server.port}")
     private String port;
+
+    // 请求url的数量
+    private int reqCount=1;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    // 模拟ribbon 轮询取模获取请求的url，RestTemplate请求调用
+    @RequestMapping("/ribbonMember")
+    public String ribbonMember(){
+        String reqUrl=getInstances()+"/getMember";
+        System.out.println("请求接口地址 reqUrl:"+reqUrl);
+        String result=restTemplate.getForObject(reqUrl,String.class);
+        return result;
+    }
+
+    // 获取轮寻的请求地址。
+    private String getInstances(){
+        // 获取eureka注册列表上SpringCloud-member的服务器集合的实例
+        List<ServiceInstance> instances= discoveryClient.getInstances("springcloud-member");
+        // 如果实例集合为空，则返回
+        if(null==instances||instances.size()<=0){
+            return  null;
+        }
+        //获取服务器集群的数量，
+        int instancesSize=instances.size();
+        // 取模运算
+        int serviceIndex= reqCount%instancesSize;
+        reqCount++;
+        // 获取请求的url
+        return instances.get(serviceIndex).getUri().toString();
+
+    }
     @RequestMapping("/getMember")
     public String getMember() {
         return "这是会员端,端口号是:"+port;
